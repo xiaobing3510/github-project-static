550f03acdc2ecef01487c3ac9d28ca557b7af2c0
chenjie <xiaobing3510@gmail.com>
2022-10-14 19:35:12 +0800

    完成了配置ajax基地址ajax拦截器以及注册功能

--- js/common.js
+++ js/common.js
@@ -7,3 +7,46 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // 每个页面都会导入该js 统一的设置写这里即可
+/**
+ * 作用:在当前页面出现一个提示消息,过一会自己就会消失
+ * @param {提示的文本} msg
+ */
+
+function tip(msg) {
+  const myToast = new bootstrap.Toast(document.querySelector('#myToast'), {
+    delay: 1000,
+  })
+  document.querySelector('#myToast .toast-body').innerHTML = msg
+  myToast.show()
+}
+
+// 配置ajax基地址
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
+
+// 添加请求拦截器
+axios.interceptors.request.use(
+  function (config) {
+    // 每次都发送请求头
+    config.headers.Authorization = sessionStorage.getItem('token')
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    return response.data
+  },
+  function (error) {
+    // token失效清除数据并退出登录
+    if (error.response.status === 401) {
+      sessionStorage.removeItem('token')
+      sessionStorage.removeItem('username')
+      location.href = './login.html'
+    }
+    return Promise.reject(error)
+  }
+)
